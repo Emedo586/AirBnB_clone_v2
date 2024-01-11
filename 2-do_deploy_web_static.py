2e1434b4e4c9008f4f18ecd9defb3c2f9e65bdb9
Emedo586 <emedopeace99@gmail.com>
2024-01-11 07:12:15 -0800

    deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["52.86.220.250", "100.25.204.218"]
 env.user = "ubuntu"
 
 
