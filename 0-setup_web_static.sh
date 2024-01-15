9536edec8cd082db92bcfbd5ca258ee07c3ca386
Emedo586 <emedopeace99@gmail.com>
2024-01-14 16:57:46 -0800

    deply static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 # Bash script that sets up a web server for deployment of web_static.
 
-apt-get update
-apt-get install -y nginx
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
 
 printf %s "server {
     listen 80 default_server;
@@ -23,7 +34,7 @@ printf %s "server {
         index index.html index.htm;
     }
     location /redirect_me {
-        return 301 http://github.com/besthor;
+        return 301 http://github.com/Emedo586;
     }
     error_page 404 /404.html;
     location /404 {
@@ -33,4 +44,4 @@ printf %s "server {
 }" > /etc/nginx/sites-available/default
 
 # Restart Nginx
-service nginx restart
+sudo service nginx restart
