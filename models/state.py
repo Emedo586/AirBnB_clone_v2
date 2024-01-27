d1fffefa68a69ca80f4117acfaabcff58c5393f9
Emedo586 <emedopeace99@gmail.com>
2024-01-27 15:27:40 -0800

    last ride with the wolves

--- models/state.py
+++ models/state.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-
-""" State Module for HBNB project
-"""
-
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from os import getenv
-from models.city import City
+"""Defines the State class."""
 import models
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class
-    Creates a relationship between State (parent) and City (child) classes.
+    """Represents a state for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
     """
-    __tablename__ = 'states'
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
 
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
-        name = ""
-
         @property
         def cities(self):
-            """getter attribute that returns the list of City instances
-            if City.state_id==current State_id
+            """Get a list of City instances with
+                state_id equals to the current State.id.
+
+            This is a getter attribute for FileStorage
+                relationship between State and City.
             """
             city_list = []
             for city in models.storage.all(City).values():
